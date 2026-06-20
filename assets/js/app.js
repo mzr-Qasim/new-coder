1244d00114a6eec0df5f119ec6d74d0cb160e96a
Qasim <malikqasim20051@gmail.com>
2026-06-20 17:43:53 +0500

    portfolio section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,16 @@
+const lightbox = GLightbox({
+  selector: '.glightbox'
+});
 
+
+
+document.querySelectorAll('.offcanvas-body .nav-link').forEach(link => {
+    link.addEventListener('click', function () {
+      const offcanvasElement = document.querySelector('.offcanvas');
+      const offcanvasInstance = bootstrap.Offcanvas.getInstance(offcanvasElement);
+
+      if (offcanvasInstance) {
+        offcanvasInstance.hide();
+      }
+    });
+  });
